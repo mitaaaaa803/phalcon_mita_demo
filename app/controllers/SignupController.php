1dee715d4684e2b51ec1fd4e467296c8915000fe
mita <a0956880803@gmail.com>
2024-08-27 18:13:35 +0800

    learning phalcon 1. Add controllers -> SignupController 2. Add views -> signup -> index.phtml

---
+++ app/controllers/SignupController.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+class SignupController extends ControllerBase
+{
+
+    public function indexAction()
+    { 
+    }
+    public function registerAction()
+    {
+        
+    }
+}
+
