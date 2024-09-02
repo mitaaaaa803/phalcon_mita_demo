94ee3478f505ec072bdc9218af68b0e9bea2ce92
mita <mitawu0803@gmail.com>
2024-09-02 11:58:12 +0800

    app/controllers/TestController.php     顯示用戶的現有資料在編輯頁面，name 和 email:         public function editAction($id)         {             $user = Users::findFirst($id);
                if (!$user) {
                $this->flashSession->error("未找到用户");
                $this->dispatcher->forward(['action' => 'index']);
                }else{
                    $this->tag->displayTo("id", $user->id);
                    $this->tag->displayTo("name", $user->name);
                    $this->tag->displayTo("email", $user->email);
                }
            }
    
    app/controllers/TestController.php
        修改資料動作:
            public function updateAction()
            {
                if (!$this->request->isPost()) {
                    $this->flashSession->error("無效的資料請求");
                } else {
                    $id = $this->request->getPost("id");
                    $user = Users::findFirst($id);// 使用這個 ID 從資料庫中查找相應的用戶。
    
                if (!$user) {
                    $this->flashSession->error("沒有該筆資料");
                } else {
                    //修改 名字 和 email
                    $user->name = $this->request->getPost("name");
                    $user->email = $this->request->getPost("email");
    
                    error_log(print_r($user->toArray(), true));
                    $success = $user->save();
    
                    if (!$success) {
                        foreach($user->getMessages() as $message) {
                            error_log("更新失敗: " . $message);
                            $this->flashSession->outputMessage("error", ("更新失敗: ".$message));
                        }
    
                        // 到編輯頁面，以便他們可以修正錯誤。
                        return $this->dispatcher->forward(array(
                            "action" => "edit",
                            "params" => array($user->id)
                        ));
                    }
    
                    //保存成功
                    $this->flashSession->success("更新成功!");
                    }
                }
                // 無論成功與否，回到首頁。
                $this->dispatcher->forward(['action' => 'index']);
            }
    
    app/views/test/edit.phtml:
        <?php echo $this->flashSession->output() ?> 在不同頁面請求之間存取和顯示消息。
        <?php echo $this->tag->form("test/update") ?>
            $this->tag->form() 用于生成表单的 HTML 標籤
            "test/update" 是表單提交的目標 URL 或路由。告訴表單數據提交到 TestController 控制器的 updateAction。

--- app/controllers/TestController.php
+++ app/controllers/TestController.php
@@ -9,11 +9,62 @@ class TestController extends \Phalcon\Mvc\Controller /* \Phalcon\Mvc\Controller 
         $this->view->users = $users;
     }
 
-    // 編輯的 Action
-    public function editAction()
+    // 顯示用戶的現有資料在編輯頁面
+    public function editAction($id) 
     {
+        $user = Users::findFirst($id);
 
+        if (!$user) {
+        $this->flashSession->error("未找到用户");
+        $this->dispatcher->forward(['action' => 'index']);
+        }else{ 
+            $this->tag->displayTo("id", $user->id);
+            $this->tag->displayTo("name", $user->name);
+            $this->tag->displayTo("email", $user->email);
+        }
     }
+
+    // 修改資料動作
+    public function updateAction() 
+    {
+        if (!$this->request->isPost()) {
+            $this->flashSession->error("無效的資料請求");
+        } else {
+            $id = $this->request->getPost("id");
+            $user = Users::findFirst($id);// 使用這個 ID 從資料庫中查找相應的用戶。
+        
+        if (!$user) {
+            $this->flashSession->error("沒有該筆資料");
+        } else {
+            //修改 名字 和 email
+            $user->name = $this->request->getPost("name");
+            $user->email = $this->request->getPost("email");
+            
+            error_log(print_r($user->toArray(), true));
+            $success = $user->save();
+
+            if (!$success) {
+                foreach($user->getMessages() as $message) { 
+                    error_log("更新失敗: " . $message);
+                    $this->flashSession->outputMessage("error", ("更新失敗: ".$message));
+                }
+                
+                // 到編輯頁面，以便他們可以修正錯誤。
+                return $this->dispatcher->forward(array(
+                    "action" => "edit",
+                    "params" => array($user->id)
+                )); 
+            }
+            
+            //保存成功
+            $this->flashSession->success("更新成功!");
+            }
+        } 
+        // 無論成功與否，回到首頁。
+        $this->dispatcher->forward(['action' => 'index']); 
+    }
+    
+     // 刪除資料動作
     public function deleteAction($id)
         {
             $users = Users::findFirst($id);
